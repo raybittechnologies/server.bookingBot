50a630626ddc9c9ad6a32dfd4d63912b1a1c7651
jasia hassan <jasiahassan120@gmail.com>
2025-11-24 15:41:00 +0530

    call data stored

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,8 @@
 // Load config from the root directory
 const { Sequelize, DataTypes } = require("sequelize");
 
-// const sequelize = new Sequelize("bookingbot", "root", "x]0TQ!4d7mS7zJ", {
-const sequelize = new Sequelize("bookingbot", "root", "root", {
+const sequelize = new Sequelize("bookingbot", "root", "x]0TQ!4d7mS7zJ", {
+  // const sequelize = new Sequelize("bookingbot", "root", "root", {
   host: "localhost",
   dialect: "mysql",
   logging: false,
